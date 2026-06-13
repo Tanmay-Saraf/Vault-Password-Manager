1298c4efb1f877d868c4048fca336e085971aca1
Tanmay <tanmaysaraf07@gmail.com>
2026-06-13 20:58:12 +0530

    Added few features

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
-import {React,useState} from 'react'
+import { React, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 
-const Card = ({item,deletePass,editPass}) => {
+const Card = ({ item, deletePass, editPass }) => {
     const [passVisi, setPassVisi] = useState('password')
-    const show = ()=>{
-        passVisi==="text"?setPassVisi("password"):setPassVisi("text")
+    const show = () => {
+        passVisi === "text" ? setPassVisi("password") : setPassVisi("text")
     }
-    const generateDot = (prop)=>{
+    const generateDot = (prop) => {
         return '•'.repeat(prop.length)
+    }
+    const copyToClipboard = (prop) => {
+        navigator.clipboard.writeText(item.password)
+        toast.success('Copied to Clipboard!')
     }
     return (
         <div className="row flex flex-col gap-4">
@@ -14,7 +19,7 @@ const Card = ({item,deletePass,editPass}) => {
                 <div className="grid grid-cols-3 gap-4 w-full">
                     <div className="webs">
                         <span className='text-xs text-neutral-500 block mb-1'>URL</span>
-                        <span className='text-white'>{item.url}</span>
+                        <span className='text-white flex items-center gap-1'><span ><img className='rounded-full' height={"18px"} width={"18px"}  src={`https://www.google.com/s2/favicons?domain=${item.url} alt=""`}/></span>{item.url}</span>
                     </div>
                     <div className="webs">
                         <span className='text-xs text-neutral-500 block mb-1'>Username</span>
@@ -23,8 +28,8 @@ const Card = ({item,deletePass,editPass}) => {
                     <div className="webs">
                         <span className='text-xs text-neutral-500 block mb-1'>Password</span>
                         <div className='flex items-center gap-2'>
-                            <span className='text-white tracking-widest'>{passVisi==='password'?generateDot(item.password):item.password}</span>
-                            <span onClick={()=>show()} className='cursor-pointer opacity-70 hover:opacity-100 transition-opacity flex items-center'>
+                            <span className='text-white tracking-widest'>{passVisi === 'password' ? generateDot(item.password) : item.password}</span>
+                            <span onClick={() => show()} className='cursor-pointer opacity-70 hover:opacity-100 transition-opacity flex items-center'>
                                 <lord-icon
                                     src="https://cdn.lordicon.com/dicvhxpz.json"
                                     trigger="click"
@@ -33,11 +38,12 @@ const Card = ({item,deletePass,editPass}) => {
                                     style={{ width: "24px", height: "24px" }}>
                                 </lord-icon>
                             </span>
+                            <span className='cursor-pointer' onClick={() => copyToClipboard(item)} title='Copy to clipboard'><i className="fa-solid fa-copy text-sm p-1"></i></span>
                         </div>
                     </div>
                 </div>
                 <div className="buttons flex gap-3 justify-end items-center">
-                    <button onClick={()=>editPass(item.id)}>
+                    <button title='edit' onClick={() => editPass(item.id)}>
                         <lord-icon
                             src="https://cdn.lordicon.com/exymduqj.json"
                             trigger="hover"
@@ -46,7 +52,7 @@ const Card = ({item,deletePass,editPass}) => {
                             style={{ width: "24px", height: "24px" }}>
                         </lord-icon>
                     </button>
-                    <button onClick={()=>deletePass(item.id)}>
+                    <button title='delete' onClick={() => deletePass(item.id)}>
                         <lord-icon
                             src="https://cdn.lordicon.com/xyfswyxf.json"
                             trigger="hover"
--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,38 +1,45 @@
-import React, { useState,useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Card from './Card.jsx'
-import {v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid"
+import { toast, ToastContainer } from 'react-toastify'
 
 const Manager = () => {
     const [visible, setVisible] = useState("password")
-    const [passwords,setPasswords] = useState(JSON.parse(localStorage.getItem('storage'))||[])
+    const [passwords, setPasswords] = useState(JSON.parse(localStorage.getItem('storage')) || [])
     useEffect(() => {
-      localStorage.setItem('storage',JSON.stringify(passwords))
+        localStorage.setItem('storage', JSON.stringify(passwords))
+        filtered = [...passwords]
     }, [passwords])
-    
+    const [search,setSearch] = useState("")
+    let filtered = passwords.filter(item => item.url.toLowerCase().includes(search.toLowerCase()))
     const username = useRef()
     const url = useRef()
     const password = useRef()
-    const showPass = ()=>{
-        visible==="text"?setVisible("password"):setVisible("text")
+    const showPass = () => {
+        visible === "text" ? setVisible("password") : setVisible("text")
     }
-    const savePass = ()=>{
+    const savePass = () => {
+        if (!url.current.value ||!username.current.value ||!password.current.value){
+            toast.error("All fields are required!");
+            return;
+        }
         const data = {
             id: uuidv4(),
             url: url.current.value,
             username: username.current.value,
             password: password.current.value
         }
-        setPasswords([...passwords,data])
+        setPasswords([...passwords, data])
         url.current.value = ""
         username.current.value = ""
         password.current.value = ""
-        alert("Password Saved")
+        toast("Password Saved!!!!")
     }
-    const deletePass = (id)=>{
-        setPasswords(passwords.filter(item=>item.id!==id))
+    const deletePass = (id) => {
+        setPasswords(passwords.filter(item => item.id !== id))
     }
-    const editPass = (id)=>{
-        const pass = passwords.find(item=>item.id===id)
+    const editPass = (id) => {
+        const pass = passwords.find(item => item.id === id)
         url.current.value = pass.url
         username.current.value = pass.username
         password.current.value = pass.password
@@ -42,9 +49,18 @@ const Manager = () => {
     }
     return (
         <main className='flex-1 flex justify-center items-center mx-auto'>
+            <ToastContainer
+                toastStyle={{
+                    background: "rgba(23,23,23,0.7)",
+                    backdropFilter: "blur(16px)",
+                    color: "white",
+                    border: "1px solid rgba(255,255,255,0.1)",
+                    borderRadius: "16px",
+                }}
+            />
             <div className=" mt-7 mb-7 w-full max-w-4xl bg-neutral-900/50 backdrop-blur-lg border border-white/10 shadow-2xl shadow-indigo-900/20 text-neutral-200 py-10 px-8 rounded-2xl flex flex-col">
-                <div className="heading flex flex-col gap-`1` justify-center items-center mb-10">
-                    <div className="name font-bold text-4xl text-white tracking-tight">Vault</div>
+                <div className="heading flex flex-col gap-1 justify-center items-center mb-10">
+                    <div className="name font-bold text-4xl text-white tracking-tight">Vau<span className='text-red-500'>lt</span></div>
                     <div className="tagline text-sm text-neutral-400">Your own Password Manager!!</div>
                 </div>
                 <div className="inputs flex flex-col gap-6 w-full max-w-2xl mx-auto mb-12">
@@ -55,13 +71,13 @@ const Manager = () => {
                     <div className='grid grid-cols-2 gap-6'>
                         <div className='flex flex-col gap-2'>
                             <label className='font-semibold text-sm text-neutral-300 ml-1 ' htmlFor="username">Username</label>
-                                <input ref={username} placeholder='Enter Username' id='username' className='bg-neutral-950/50 border border-neutral-700 focus:border-red-500 focus:ring-1 focus:ring-red-500 outline-none w-full rounded-xl text-white py-2 px-4 text-lg transition-all duration-200' type="text" />
+                            <input ref={username} placeholder='Enter Username' id='username' className='bg-neutral-950/50 border border-neutral-700 focus:border-red-500 focus:ring-1 focus:ring-red-500 outline-none w-full rounded-xl text-white py-2 px-4 text-lg transition-all duration-200' type="text" />
                         </div>
                         <div className='flex flex-col gap-2'>
                             <label className='font-semibold text-sm text-neutral-300 ml-1 ' htmlFor="password">Password</label>
                             <div className='relative w-full'>
                                 <input ref={password} placeholder='Enter Password' id='password' className='bg-neutral-950/50 border border-neutral-700 focus:border-red-500 focus:ring-1 focus:ring-red-500 outline-none rounded-xl text-white py-2 pl-4 pr-12 text-lg w-full transition-all duration-200' type={visible} />
-                                <span onClick={()=>showPass()} className='absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer opacity-70 hover:opacity-100 transition-opacity flex items-center'>
+                                <span onClick={() => showPass()} className='absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer opacity-70 hover:opacity-100 transition-opacity flex items-center'>
                                     <lord-icon
                                         src="https://cdn.lordicon.com/dicvhxpz.json"
                                         trigger="click"
@@ -73,7 +89,7 @@ const Manager = () => {
                             </div>
                         </div>
                     </div>
-                    <button onClick={()=>savePass()} className='mt-4 bg-white/10 hover:bg-white/20 border border-white/20  shadow-lg backdrop-blur-md text-white transition-all duration-200 mx-auto py-3 px-8 rounded-full font-bold flex justify-center items-center gap-2 cursor-pointer w-fit'>
+                    <button onClick={() => savePass()} className='mt-4 bg-white/10 hover:bg-white/20 border border-white/20  shadow-lg backdrop-blur-md text-white transition-all duration-200 mx-auto py-3 px-8 rounded-full font-bold flex justify-center items-center gap-2 cursor-pointer w-fit'>
                         <lord-icon
                             src="https://cdn.lordicon.com/efxgwrkc.json"
                             trigger="hover"
@@ -87,10 +103,16 @@ const Manager = () => {
                 <div className='w-full h-[1px] bg-white/10 mb-8'></div>
 
                 <div className="rows flex flex-col w-full max-w-3xl mx-auto">
-                    <h2 className='font-bold text-2xl mb-6 text-white'>Stored Passwords</h2>
-                    {passwords.length===0 && <div>No Passwords Stored yet!</div>}
-                    {passwords.length!==0 && passwords.map(item=>{
-                        return <Card key={item.id} item = {item} deletePass={deletePass} editPass={editPass} />
+                    <div className='flex items-center  mb-6 justify-between'>
+                        <h2 className='font-bold text-2xl  text-white'>Stored Passwords</h2>
+                        <div className='relative flex justify-center items-center'>
+                            <i className="absolute left-2 fa-solid fa-magnifying-glass"></i>
+                            <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder='Search by URL' className='bg-neutral-950/50 border border-neutral-700 focus:border-red-500 focus:ring-1 focus:ring-red-500 outline-none w-full rounded-xl text-white py-1 px-10 text-sm transition-all duration-200'/>
+                        </div>
+                    </div>
+                    {passwords.length === 0 && <div>No Passwords Stored yet!</div>}
+                    {passwords.length !== 0 && filtered.map(item => {
+                        return <Card key={item.id} item={item} deletePass={deletePass} editPass={editPass} />
                     })}
                 </div>
             </div>
